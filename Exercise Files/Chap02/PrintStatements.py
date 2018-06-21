e9ea43ea649adb8ab48e53ae4dd86eaf15834a7d
sanoobtv <sanoob.tv@gmail.com>
2018-06-21 22:08:24 +1000

    Initial Commit

---
+++ Exercise Files/Chap02/PrintStatements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+x = ' seven {1:>09} {0:<09} ' .format (8,9)
+print ( x , '{}' .format (5))
+print(type(x))
+a=1
+b=2
+
+z = f'another number {b} '
+
+print (z)
---
+++ Exercise Files/Chap03/types.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+from decimal import *
+a= Decimal ('.1')
+b= Decimal ('.3')
+x = True
+print('x is {}'.format(x))
+print(type(x))
